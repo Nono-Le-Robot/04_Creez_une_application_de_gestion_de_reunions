1a568359c718cd1e84469018fb267a79803bb92f
Nono-Le-Robot <87859199+Nono-Le-Robot@users.noreply.github.com>
2023-12-08 17:07:30 +0100

    init

---
+++ MaReu/app/src/main/java/com/example/mareu/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.mareu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mareu.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.inputtext.setHint("entrez un texte");
+        binding.text.setText("ookokokokok");
+    }
+}
